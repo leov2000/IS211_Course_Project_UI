4061f780422a38e1afcc2fe3f7b486b304c1afc5
leo <leovergarah@gmail.com>
2019-12-08 00:10:49 -0500

    (Added) conditional rendering

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react';
 import Blog from '../blog';
 import axios from 'axios';
+import { uniqueId, isEmpty } from 'lodash';
 
 export default class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            blogList: []
+        }
+    }
 
     componentDidMount() {
-        axios.get('/posts').then(res => console.log(res))
+        axios
+            .get('/posts')
+            .then(res => {
+                const { data } = res;
+
+                this.setState({blogList: data});
+            });
     }
 
     render() {
+        const { blogList } = this.state;
         return (
             <div className="blogs-view">
-                <Blog/>
-                <Blog/>
-                <Blog/>
-                <Blog/>
-                <Blog/>
-                <Blog/>
+                {
+                    isEmpty(blogList) ? <h2 className="no-posts"> No Posts </h2> : blogList.flatMap(obj => obj.isHidden  === "true" ? [] : <Blog key={uniqueId()} {...obj}/>)
+                }
             </div>
         );
     } 
