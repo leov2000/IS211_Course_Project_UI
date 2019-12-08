c03a0c62966a74c7b902330ed5a24eb44ccd1057
leo <leovergarah@gmail.com>
2019-12-07 20:56:08 -0500

    (Updates) to main component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import Landing from './components/landing';
+import './assets/styles/app.css';
 
 class App extends Component {
   render() {
