b7d69d39cea0d180cdcd6211f3600de78c05b4f8
leo <leovergarah@gmail.com>
2019-12-08 00:11:20 -0500

    (Added) config for photos

---
+++ src/config/photo-config.js
@@ -0,0 +1,12 @@
+export default {
+    sports: 'https://imagesvc.timeincapp.com/v3/mm/image?url=https://cdn-s3.si.com/images/football-pylon.jpg&w=400&h=300&c=sc&poi=face&q=85',
+    lifestyle: 'https://www.rifemagazine.co.uk/wp-content/uploads/2019/10/martyna-bober-AeLUd79Rjoo-unsplash-400x300.jpg',
+    politics: 'https://news.psu.edu/sites/default/files/styles/gss_thumbnail/public/800px-US_Flag_Backlit.jpg',
+    fashion: 'https://cf.ltkcdn.net/fashion-history/images/std/169251-400x300-fashion-poster-ads.jpg',
+    architecture: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Feuerwehrhaus_von_Zaha_Hadid_bei_Vitra_in_Weil_am_Rhein_an_der_%C3%81lvaro-Siza-Promenade_2.jpg/400px-Feuerwehrhaus_von_Zaha_Hadid_bei_Vitra_in_Weil_am_Rhein_an_der_%C3%81lvaro-Siza-Promenade_2.jpg',
+    local: 'https://www.redfirefarm.com/wp-content/uploads/2016/02/springfieldfarmersmarketIMG_4363edit-400x300.jpg',
+    eats: 'https://dsu113jcnntrj.cloudfront.net/wp-content/uploads/2015/07/DSC_0028-e1438796605578-400x300.jpg',
+    home:'https://www.cliqstudios.com/images/Sample-Box-Rear-Render-6-400x300.jpg',
+    finance: 'https://sgp1.digitaloceanspaces.com/cas-img/wp-content/uploads/2019/07/13091322/wall-street-bull-400x300.jpg',
+    fallback: 'http://www.ryukyu.ac.jp/wp-content/uploads/2012/09/Tulips-400x300.jpg'
+}
