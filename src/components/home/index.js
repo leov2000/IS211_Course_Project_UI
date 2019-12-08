c337f4e572863e724de4c74ebdd51856d1563529
leo <leovergarah@gmail.com>
2019-12-07 21:51:46 -0500

    (Added) config & utility folder

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import Blog from '../blog';
+import axios from 'axios';
 
 export default class Home extends Component {
+
+    componentDidMount() {
+        axios.get('/posts').then(res => console.log(res))
+    }
 
     render() {
         return (
