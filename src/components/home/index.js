e0e49bcc1c2756ceadb7870ba94b3cae0b84c514
leo <leovergarah@gmail.com>
2019-12-08 00:40:41 -0500

    (Added) nav portion

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import Blog from '../blog';
 import axios from 'axios';
-import { uniqueId, isEmpty } from 'lodash';
+import { uniqueId, isEmpty, capitalize } from 'lodash';
+import navConfig from '../../config/nav-config';
 
 export default class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            blogList: []
+            blogList: [],
+            active: ''
         }
     }
 
@@ -23,8 +25,15 @@ export default class Home extends Component {
 
     render() {
         const { blogList } = this.state;
+        console.log(this.state, 'THE STATE')
+        console.log(navConfig, 'NAV-CONFIG')
         return (
             <div className="blogs-view">
+                <div className="blog-nav-bar">
+                {
+                    navConfig.map(nav => <h3>{nav}</h3>)
+                }
+                </div>
                 {
                     isEmpty(blogList) ? <h2 className="no-posts"> No Posts </h2> : blogList.flatMap(obj => obj.isHidden  === "true" ? [] : <Blog key={uniqueId()} {...obj}/>)
                 }
