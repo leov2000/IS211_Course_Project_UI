a015da7a7e307ab239c03508c34257f1cdbbe7cc
Leo <leovergarah@gmail.com>
2019-12-14 16:50:48 -0500

    (Added) form state & error styling

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -6,43 +6,61 @@ export default class Login extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            form: {
-                user: ''
-            }
+            user: '',
+            password: '',
+            error: false
         };
     }
 
-    submitFormRequest(values) {
-        console.log(values, 'values here')
-        values.preventDefault();
+    submitFormRequest() {
+        const { user, password } = this.state;
+    }
+
+    handleUserInput(event) {
+        this.setState(
+            { user: event.target.value }
+        );
+    }
+
+    handlePasswordInput(event) {
+        this.setState(
+            { password: event.target.value }
+        );
     }
 
     render() {
+        const { user, password } = this.state;
+
         return (
             <div className="login-container">
                 <div className="login-form-container">
-                    <h2>Login or Sign up!
-                        <span className="mini-header-under"></span>
-                    </h2>
-                    <form name="loginForm" onSubmit={this.submitFormRequest}>
+                    {
+                        !this.state.error ? 
+                        (<h2>Login or Sign up!
+                            <span className="mini-header-under"></span>
+                        </h2>) : 
+                        (<h2 className="form-login-error">Error! Try again!
+                            <span className="mini-header-under-error"></span>
+                        </h2>)
+                    }
+                    <form name="loginForm" onSubmit={() => this.submitFormRequest()}>
                         <div>
-                            <div className='input-container'>
+                            <div className="input-container">
                                 <div>
                                     <span>Username</span>
                                 </div>
-                                <input type="text"/>
+                                <input type="text" name="user" value={user} onChange={(event) => this.handleUserInput(event)}/>
                             </div>
-                            <div className='input-container'>
+                            <div className="input-container">
                                 <div>
                                     <span>Password</span>
                                 </div>
-                                <input type="password"/>
+                                <input type="password" name="password" value={password} onChange={(event) => this.handlePasswordInput(event)}/>
                             </div>
                         </div>
                         <div className="form-submit-button">
-                            <span>SUBMIT</span>
+                            <span onClick={() => this.submitFormRequest()}>SUBMIT</span>
                         </div>
-                        {/* <button type="button" className="form-submit-button">SUBMIT</button> */}
                     </form>
                 </div>
             </div>
