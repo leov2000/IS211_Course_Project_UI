b96e44e8c1cc3892ba4d15f3fb212dc796647a20
leo <leovergarah@gmail.com>
2019-12-14 15:18:12 -0500

    (Added) layout for login component

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -4,7 +4,30 @@ export default class Login extends Component {
 
     render() {
         return (
-            <h2>Login</h2>
+            <div className="login-container">
+                <div className="form-container">
+                    <h2>Login or Sign up!
+                        <span className="mini-header-under"></span>
+                    </h2>
+                    <form name="loginForm">
+                        <div>
+                            <div className='input-container'>
+                                <div>
+                                    <span>Username</span>
+                                </div>
+                                <input type="text"/>
+                            </div>
+                            <div className='input-container'>
+                                <div>
+                                    <span>Password</span>
+                                </div>
+                                <input type="password"/>
+                            </div>
+                        </div>
+                        <button className="form-submit-button">SUBMIT</button>
+                    </form>
+                </div>
+            </div>
         );
     } 
 }
