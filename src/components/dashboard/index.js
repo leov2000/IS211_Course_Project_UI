5aa6446134df0652706ae683d1d15aadbe4d701b
Leo <leovergarah@gmail.com>
2019-12-14 18:54:24 -0500

    (Added) conditional landing rendering when a user logs in through the login

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
+import { get } from 'lodash';
 
 export default class Dashboard extends Component {
     render() {
-        return (<h2>DASHBOARD HERE</h2>);
+        const { location } = this.props;
+        const { state } = location;
+        const user = get(state, 'user', false);
+
+        return (
+            <div>
+                {
+                    !user ? 
+                    (<h2>SOMETHING WEIRD HAPPEN. PLEASE LOGIN FROM <a href="/login">login</a></h2>) :
+                    (<h2>DASHBOARD HERE</h2>)
+                }
+            </div>
+        );
     }
 }
