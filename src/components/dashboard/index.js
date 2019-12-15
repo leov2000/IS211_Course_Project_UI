675d70da5ef19cecedcf993e2bd444132f975075
Leo <leovergarah@gmail.com>
2019-12-15 16:16:13 -0500

    (Added) scaffolding state for form and form state reset

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -9,7 +9,14 @@ export default class Dashboard extends Component {
         super(props);
         this.state = {
             blogPosts: [],
-            open: true 
+            open: false,
+            formValues: {
+                title: '',
+                category: '',
+                isHidden: '',
+                pub_date: '',
+                content: ''
+            }
         };
     }
 
@@ -23,13 +30,71 @@ export default class Dashboard extends Component {
                 .get('/admin', { params: { user } })
                 .then(res => {
                     const { data } = res;
-                    
-                    this.setState({blogPosts: data});
+
+                    this.setState({ blogPosts: data });
                 });
         }
     }
-    editBlogPost() {
-        console.log('edit');
+
+    handleChange(event) {
+        const { target } = event;
+        const { name, value } = target;
+        const { formValues } = this.state;
+
+        this.setState({ formValues: {...formValues, [name]: value} });
+    }
+
+    clearFormState() {
+        const { formValues } = this.state;
+        const formKeys = Object.keys(formValues);
+        const resetValues = formKeys.reduce((prev, curr) => ({...prev, [curr]: ''}), {});
+
+        return resetValues;
+    }
+
+    blogTemplate() {
+        return (
+            <div className="modal-container">
+                <div className="modal-header">
+                    <h1 className="modal-title">New Blog</h1>
+                    <span className="modal-close-icon" onClick={() => this.closeModal()}>X</span>
+                </div>
+                <div className="modal-form">
+                    <div>
+                        <span>Title</span>
+                        <input className="modal-input" type="text" name="title" onChange={(e) => this.handleChange(e)} />
+                    </div>
+                    <div className="modal-drop-down-container">
+                        <div className="modal-drop-down-item">
+                            <span>Category</span>
+                            <select className="modal-drop-down" name="category" onChange={(e) => this.handleChange(e)}>
+                                {
+                                    categoryConfig.map(category => <option key={uniqueId()}  value={category.toLowerCase()}>{category}</option>)
+                                }
+                            </select>
+                        </div>
+                        <div className="modal-drop-down-item">
+                            <span>Hidden</span>
+                            <select className="modal-drop-down" name="isHidden" onChange={(e) => this.handleChange(e)}>
+                                <option  value="true">True</option>
+                                <option value="false">False</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div>
+                        <span>Publish Date</span>
+                        <input className="modal-input" name="pub_date" type="date" onChange={(e) => this.handleChange(e)}/>
+                    </div>
+                    <div>
+                        <span>Content</span>
+                        <input className="modal-input" name="content" type="text" onChange={(e) => this.handleChange(e)}/>
+                    </div>
+                </div>
+                <div className="modal-button">
+                    <span className="admin-submit-button">SUBMIT</span>
+                </div>
+            </div>
+        );
     }
 
     deleteBlogPost() {
@@ -43,8 +108,11 @@ export default class Dashboard extends Component {
     }
 
     closeModal() {
+        const formValues = this.clearFormState();
+
         this.setState({
-            open: false
+            open: false,
+            formValues
         });
     }
 
@@ -55,102 +123,66 @@ export default class Dashboard extends Component {
         const { blogPosts } = this.state;
         console.log(blogPosts, 'BLOGPOSTS HERE');
         console.log(this.state, 'THE STATE HERE');
+
         return (
             <div>
                 <Popup
                     open={this.state.open}
                     onClose={() => this.closeModal()}
-                    contentStyle={{ 
+                    contentStyle={{
                         padding: "0px",
                         border: "1px white solid",
                         minHeight: "500px",
-                        background: "#36D1DC"  
-                     }}
+                        background: "#36D1DC"
+                    }}
                 >
-                    <div className="modal-container">
-                        <div className="modal-header">
-                            <h1 className="modal-title">Edit</h1>
-                            <span className="modal-close-icon" onClick={() => this.closeModal()}>X</span>
-                        </div>
-                        <div className="modal-form">
-                            <div>
-                                <span>Title</span>
-                                <input className="modal-input" type="text"/>
-                            </div>
-                            <div className="modal-drop-down-container">
-                                <div className="modal-drop-down-item">
-                                    <span>Category</span>
-                                    <select className="modal-drop-down">
-                                        {
-                                            categoryConfig.map(category => <option key={uniqueId()} value={category.toLowerCase()}>{category}</option>)
-                                        }
-                                    </select>
-                                </div>
-                                <div className="modal-drop-down-item">
-                                    <span>Hidden</span>
-                                    <select className="modal-drop-down">
-                                        <option value="true">True</option>
-                                        <option value="false">False</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div>
-                                <span>Publish Date</span>
-                                <input className="modal-input" type="date"/>
-                            </div>
-                            <div>
-                                <span>Content</span>
-                                <input className="modal-input" type="text"/>
-                            </div>
-                        </div>
-                        <div className="modal-button">
-                            <span className="admin-submit-button">SUBMIT</span>
-                        </div>
-                    </div>
-                </Popup>   
+                    {
+                        this.blogTemplate()
+                    }
+                </Popup>
                 {
-                    !user ? 
-                    (<h2>SOMETHING WEIRD HAPPEN. PLEASE LOGIN FROM <a href="/login">login</a></h2>) :
-                    (
-                        <div className="dashboard-pane">
-                            <div className="admin-add-blog-container">
-                                <span className="admin-add-blog" onClick={() => this.showModal()}>
-                                    <h3>
-                                        Add Blog
+                    !user ?
+                        (<h2>SOMETHING WEIRD HAPPEN. PLEASE LOGIN FROM <a href="/login">login</a></h2>) :
+                        (
+                            <div className="dashboard-pane">
+                                <div className="admin-add-blog-container">
+                                    <span className="admin-add-blog" onClick={() => this.showModal()}>
+                                        <h3>
+                                            Add Blog
                                     </h3>
-                                </span>
+                                    </span>
+                                </div>
+                                <div className="admin-blog-container">
+                                    <table className="admin-blog-table">
+                                        <caption><h2>{user} Blog Posts</h2></caption>
+                                        <thead>
+                                            <tr>
+                                                <th>Title</th>
+                                                <th>Content</th>
+                                                <th>Post Hidden</th>
+                                                <th>Publish Date</th>
+                                                <th>Category</th>
+                                                <th>Edit</th>
+                                                <th>Delete</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {blogPosts.map(blog => (
+                                                <tr key={uniqueId()}>
+                                                    <td className="content-td" width="200" key={uniqueId()}> {blog.title} </td>
+                                                    <td className="content-td" width="200" key={uniqueId()}> {blog.content} </td>
+                                                    <td width="150" key={uniqueId()}> {blog.isHidden} </td>
+                                                    <td width="150" key={uniqueId()}> {blog.pub_date} </td>
+                                                    <td width="150" key={uniqueId()}> {blog.category} </td>
+                                                    <td width="150" onClick={() => this.editBlogPost()} ><span>Edit</span></td>
+                                                    <td width="150" onClick={() => this.deleteBlogPost()} ><span>Delete</span></td>
+                                                </tr>
+                                            ))}
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
-                            <div className="admin-blog-container">
-                                <table className="admin-blog-table">
-                                    <caption><h2>{user} Blog Posts</h2></caption>
-                                    <thead>
-                                        <tr>
-                                            <th>Title</th>
-                                            <th>Content</th>
-                                            <th>Post Hidden</th>
-                                            <th>Publish Date</th>
-                                            <th>Category</th>
-                                            <th>Edit</th>
-                                            <th>Delete</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                     { blogPosts.map(blog => (
-                                         <tr key={uniqueId()}>
-                                             <td className="content-td" width="200" key={uniqueId()}> {blog.title} </td>
-                                             <td className="content-td" width="200" key={uniqueId()}> {blog.content} </td>
-                                             <td width="150" key={uniqueId()}> {blog.isHidden} </td>
-                                             <td width="150" key={uniqueId()}> {blog.pub_date} </td>
-                                             <td width="150"key={uniqueId()}> {blog.category} </td>
-                                             <td width="150" onClick={() => this.editBlogPost()} ><span>Edit</span></td>
-                                             <td width="150" onClick={() => this.deleteBlogPost()} ><span>Delete</span></td>
-                                         </tr>    
-                                        )) }
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>    
-                    )
+                        )
                 }
             </div>
         );
